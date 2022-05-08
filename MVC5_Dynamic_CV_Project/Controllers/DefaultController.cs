5cf158e0d59754e559560bbfedcf5d351c9cd995
Berkay Serpek <berkay.serpek@toptalent.co>
2022-05-08 23:09:08 +0300

    Add project files.

---
+++ MVC5_Dynamic_CV_Project/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using MVC5_Dynamic_CV_Project.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5_Dynamic_CV_Project.Controllers
+{
+    public class DefaultController : Controller
+    {
+        DbCVEntities db = new DbCVEntities();
+        // GET: Default
+        public ActionResult Index()
+        {
+            var values = db.Abouts.ToList();
+            return View(values);
+        }
+    }
+}
---
+++ MVC5_Dynamic_CV_Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC5_Dynamic_CV_Project.MvcApplication" Language="C#" %>
