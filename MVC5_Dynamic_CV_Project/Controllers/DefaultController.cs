032d3476272a7b7b8d058a49ecef0e2a72304e56
Berkay Serpek <berkay.serpek@toptalent.co>
2022-05-13 16:26:30 +0300

    Entities list as default

--- MVC5_Dynamic_CV_Project/Controllers/DefaultController.cs
+++ MVC5_Dynamic_CV_Project/Controllers/DefaultController.cs
@@ -16,5 +16,31 @@ namespace MVC5_Dynamic_CV_Project.Controllers
             var values = db.Abouts.ToList();
             return View(values);
         }
+
+        public PartialViewResult Experience()
+        {
+            var experiences = db.Experiences.ToList();
+            return PartialView(experiences);
+        }
+        public PartialViewResult Education()
+        {
+            var educations = db.Educations.ToList();
+            return PartialView(educations);
+        }
+        public PartialViewResult Skill()
+        {
+            var skills = db.Skills.ToList();
+            return PartialView(skills);
+        }
+        public PartialViewResult Hobbie()
+        {
+            var hobbies = db.Hobbies.ToList();
+            return PartialView(hobbies);
+        }
+        public PartialViewResult Certifica()
+        {
+            var certificas = db.Certificas.ToList();
+            return PartialView(certificas);
+        }
     }
 }
